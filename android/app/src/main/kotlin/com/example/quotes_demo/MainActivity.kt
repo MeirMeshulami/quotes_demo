37272602e2c4f68dbf487eb93ad4321bacc0b5e4
Meir Meshulami <meir.meshoulami@gmail.com>
2024-06-25 22:25:37 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/quotes_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quotes_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const Placeholder());
+// }
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Initialization Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Initialization'),
+      ),
+      body: Center(
+        child: Text('Firebase is initialized!'),
+      ),
+    );
+  }
+}
